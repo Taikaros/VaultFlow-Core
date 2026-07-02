f162e04eceac8dd3b82b14f6ebb13e8a4860cc0c
Hugo A Gabriel Sosa <gabrielss365@gmail.com>
2026-07-02 14:58:55 -0300

    ci(commitlint): add commitlint + husky for conventional commits enforcement
    - Add @commitlint/cli and @commitlint/config-conventional
    - Add husky with commit-msg hook
    - Create commitlint.config.js with Conventional Commits rules
    - Add root package.json for shared tooling
    
    Closes #3

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+  },
+}
